228c84b75e2dfe3e5651eaf8b7319d04f5f1a27f
xarvh <francesco.orsenigo@gmail.com>
2017-09-30 13:09:41 +1000

    wip

--- src/Collision.elm
+++ src/Collision.elm
@@ -4,27 +4,134 @@ import Math
 import Math.Vector2 as Vec2 exposing (Vec2, vec2)
 
 
-vecToBase : Vec2 -> ( Vec2, Vec2 )
-vecToBase v =
-    let
-        e1 =
-            Vec2.normalize v
+{-
+   vecToBase : Vec2 -> ( Vec2, Vec2 )
+   vecToBase v =
+       let
+           e1 =
+               Vec2.normalize v
 
-        e2 =
-            Math.rotate90 e1
-    in
-        ( e1, e2 )
-
-
-segmentToBase : Vec2 -> Vec2 -> ( Vec2, Vec2 )
-segmentToBase a b =
-    Vec2.sub b a |> vecToBase
+           e2 =
+               Math.rotate90 e1
+       in
+           ( e1, e2 )
 
 
-{-| -}
+   segmentToBase : Vec2 -> Vec2 -> ( Vec2, Vec2 )
+   segmentToBase a b =
+       Vec2.sub b a |> vecToBase
+-}
+
+
+{-| Checks if a trajectory collides with a point
+
+*) r: collision radius
+
+*) a: point
+
+*) c: trajectory start
+
+*) d: trajectory end
+
+
+The circumference of radius `r` centered in `a` and the straight line
+passing through `c` and `d` are described by two equations.
+
+Collisions happen where the two equations intersect.
+
+Intersection points can be found by solving the system of the two equations,
+which yields a second grade polynomial.
+The polynomial can have:
+* zero solutions: the objects are too far, no collision
+* one solution: the objects barely touch, no collision
+* two solutions: the objects overlap, collision must be resolved
+
+The math becomes much easier if we resolve it in a coordinate system where:
+* cd is horizontal and going from left to right
+* a is at the origin
+
+        ^
+        |
+  c --s1--s2-----------------> d
+        |
+        |
+        |
+        |
+        a----------------------------->
+
+
+  Equation for cd:
+
+      y = cY
+
+  Equation for the circumference:
+
+      x^2 + y^2 = r^2
+
+  The above have solutions:
+
+      y = cY
+      x = +- sqrt(r^2 - cY^2)
+
+  Which means that collisions happen if and only if
+
+    r^2 > cY^2
+
+  Also, if solutions are present, we can take the one with the negative sign, as it is the one
+  closer to the trajectory start.
+
+
+-}
 pointToPoint : Float -> Vec2 -> ( Vec2, Vec2 ) -> Maybe ( Vec2, Vec2 )
 pointToPoint r a ( c, d ) =
-    Nothing
+    let
+        -- coordinate transform: a is the origin
+        c_a =
+            Vec2.sub c a
+
+        d_a =
+            Vec2.sub d a
+
+        -- coordinate transform: cd is horizontal
+        -- since we do NOT normalise x and y, this transform changes the metric of our space
+        x =
+            Vec2.sub d_a c_a
+
+        y =
+            Math.rotate90 x
+
+        xx =
+            Vec2.lengthSquared x
+
+        yy =
+            xx
+
+        -- r^2, with the new metric
+        rr_ =
+            r * r / xx
+
+        -- cY, with the new metric and origin
+        --
+        -- cY_ = (Vec2.dot c_a y) / Vec2.length y
+        --
+        -- cY_^2
+        cYcY_ =
+            let
+                co =
+                    Vec2.dot c_a y
+            in
+                co * co / yy
+    in
+        if rr <= cYcY then
+            Nothing
+        else
+          let
+
+
+
+
+          in
+
 
 
 {-| Checks if a trajectory collides with an oriented segment.
