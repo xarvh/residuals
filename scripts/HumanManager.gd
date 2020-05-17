565d2e33868f73447ccf597d8f703aa28ccabac7
xarvh <francesco.orsenigo@gmail.com>
2020-05-17 12:03:05 +0200

    Have cell highlight

--- scripts/HumanManager.gd
+++ scripts/HumanManager.gd
@@ -18,11 +18,14 @@ const walkingSpeed = 10
 # internals
 #
 var tilemap
+var cellHighlight
 var sprite
 var animation_player
 
 func _ready():
     self.tilemap = self.get_node('TileMap')
+    self.cellHighlight = self.tilemap.get_node('CellHighlight')
+    self.cellHighlight.visible = false
     self.sprite = self.get_node('HumanCharacter')
     self.animation_player = self.sprite.get_node("AnimationPlayer")
 
@@ -32,13 +35,10 @@ func _ready():
 #
 func _unhandled_input(event):
 
-    if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
-        # TODO initiate action
-        print("PRESSED")
+    if event.is_pressed() and InputMap.event_is_action(event, inputUseTool):
+        self.cellHighlight.visible = true
 
-    var tileCoordinates = self.tilemap.world_to_map(self.tilemap.get_local_mouse_position())
 
-#    print("lol", event)
 
 
 #
@@ -49,15 +49,29 @@ func _process(delta):
     if Input.is_action_just_pressed(inputQuit):
         get_tree().quit()
 
-    var a = self.animation_player.current_animation
+    var animation = self.animation_player.current_animation
 
-    if a == "SwingTool":
+    if animation == "SwingTool":
       return
 
-    if Input.is_action_just_pressed(inputUseTool):
-        if a == "Idle" or a == "Walk":
-          var x = self.animation_player.play("SwingTool")
+
+    if self.cellHighlight.visible:
+      self.cellHighlight.rect_position = self.tilemap.world_to_map(self.tilemap.get_local_mouse_position()) * self.tilemap.cell_size
+
+
+
+    if Input.is_action_just_released(inputUseTool):
+        self.cellHighlight.visible = false
+        if animation == "Idle" or animation == "Walk":
+          self.animation_player.play("SwingTool")
           return
+
+
+
+
+
+
+
 
     var dx = -1 if Input.is_action_pressed(inputLeft) else 1 if Input.is_action_pressed(inputRight) else 0
     var dy = -1 if Input.is_action_pressed(inputUp) else 1 if Input.is_action_pressed(inputDown) else 0
@@ -67,6 +81,7 @@ func _process(delta):
         move(dx / n, dy / n, delta)
     else:
         stand(delta)
+
 
 func move(dx, dy, delta):
     self.sprite.position.x += dx * delta * walkingSpeed
